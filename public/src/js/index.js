570ac9fa6ec915c7eee01c515d1fac8e6708f157
luke01455 <luke0145555@hotmail.co.uk>
2020-02-04 16:43:56 +0000

    final changed made to project

---
+++ public/src/js/index.js
@@ -0,0 +1,30 @@
+
+// toggle the hamburger menu
+(function() {
+
+    var hamburger = {
+      navToggle: document.querySelector('.nav-toggle'),
+      nav: document.querySelector('nav'),
+  
+      doToggle: function(e) {
+        e.preventDefault();
+        this.navToggle.classList.toggle('expanded');
+        this.nav.classList.toggle('expanded');
+      }
+    };
+  
+    hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+    hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
+  
+  }());
+
+  // smooth scroll to an id from href
+  $(document).ready(function() {
+    // on clicking any href which has a hash
+    $('a[href*=\\#]').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop : $(this.hash).offset().top
+        }, 500);
+    });
+});
